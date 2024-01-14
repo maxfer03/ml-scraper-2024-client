1fbd40578440db536e58a5125b6f11de6f09cf25
Max <sandsferdinand@gmail.com>
2024-01-14 20:28:57 -0300

    updated search function to fit new api structure + added current / total page visualization

--- src/components/ContentViewerComponent.vue
+++ src/components/ContentViewerComponent.vue
@@ -6,6 +6,7 @@
     </div>
     <SpinnerComponent v-if="loading" />
     <div v-if="products.length > 0" class="products-visualizer">
+      <span v-if="loading">page {{ reqInfo.current_page }} of {{ reqInfo.total_pages }}</span>
       <span>{{ products.length }} item{{products.length === 1 ? '' : 's'}} found!</span>
       <div class="files">
         <Button @click="downloadCSV"  label="Download CSV file" rounded size="small"/>
@@ -31,12 +32,22 @@ import { ref } from 'vue';
 import axios from 'axios';
 const query = ref('')
 const loading = ref(false)
+
+const reqInfo = ref ({
+  current_page: 0,
+  total_pages: null,
+  url: ''
+})
 const products = ref([])
 
 const search = async () => {
   products.value = []
+  reqInfo.value = {
+  current_page: 0,
+  total_pages: null,
+  url: ''
+}
   loading.value = true
-  let counter = 0
   let oopsError = 0
   while (oopsError === 0) {
     try {
@@ -45,12 +56,15 @@ const search = async () => {
         params: 
         { 
           query: query.value,
-          page: counter
+          page: reqInfo.value.current_page
         } 
       });
       console.log(response)
-      products.value = [...products.value, ...response.data]
-      counter = counter + 1
+      products.value = [...products.value, ...response.data.products]
+      if(!reqInfo.value.total_pages) {
+        reqInfo.value.total_pages = response.data.info.total_pages
+      }
+      reqInfo.value.current_page++
     } catch (error) {
       oopsError = 1
       console.error(error); // Handle the error here
