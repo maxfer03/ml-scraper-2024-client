9fb077d4ef59cb9d27ce0cf97f4d8ddd60da423a
Max <sandsferdinand@gmail.com>
2024-01-14 21:07:04 -0300

    added data table

--- src/components/ContentViewerComponent.vue
+++ src/components/ContentViewerComponent.vue
@@ -6,19 +6,13 @@
     </div>
     <SpinnerComponent v-if="loading" />
     <div v-if="products.length > 0" class="products-visualizer">
+      <span v-if="loading">page {{ reqInfo.current_page }} of {{ reqInfo.total_pages }}</span>
       <span>{{ products.length }} item{{products.length === 1 ? '' : 's'}} found!</span>
       <div class="files">
         <Button @click="downloadCSV"  label="Download CSV file" rounded size="small"/>
         <Button @click="downloadXLSX"  label="Download XLSX file" rounded size="small"/>
       </div>
-      <!-- <ul>
-        <li v-for="product, idx in products" :key="`prod-${idx}`">
-          <a :href="product.url" target="_blank" >
-            {{ product.title }}
-          </a>
-          <span>$ {{ product.final_price }}</span>
-        </li>
-      </ul> -->
+      <TableComponent :data="products"/>
     </div>
   </div>
 </template>
@@ -26,17 +20,28 @@
 <script setup>
 import InputText from 'primevue/inputtext';
 import SpinnerComponent from './utility/SpinnerComponent.vue';
+import TableComponent from './TableComponent.vue';
 import * as XLSX from 'xlsx/xlsx.mjs';
 import { ref } from 'vue';
 import axios from 'axios';
 const query = ref('')
 const loading = ref(false)
+
+const reqInfo = ref ({
+  current_page: 0,
+  total_pages: null,
+  url: ''
+})
 const products = ref([])
 
 const search = async () => {
   products.value = []
+  reqInfo.value = {
+  current_page: 0,
+  total_pages: null,
+  url: ''
+}
   loading.value = true
-  let counter = 0
   let oopsError = 0
   while (oopsError === 0) {
     try {
@@ -45,12 +50,15 @@ const search = async () => {
         params: 
         { 
           query: query.value,
-          page: counter
+          page: reqInfo.value.current_page
         } 
       });
       console.log(response)
-      products.value = [...products.value, ...response.data]
-      counter = counter + 1
+      products.value = [...products.value, ...response.data.products]
+      if(!reqInfo.value.total_pages) {
+        reqInfo.value.total_pages = response.data.info.total_pages
+      }
+      reqInfo.value.current_page++
     } catch (error) {
       oopsError = 1
       console.error(error); // Handle the error here
---
+++ src/components/TableComponent.vue
@@ -0,0 +1,26 @@
+<template>
+<DataTable :value="data" paginator :rows="25" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="width: 1000px"
+scrollable scrollHeight="400px"
+>
+    <Column field="title" header="Title" sortable style="width: 25%"></Column>
+    <Column field="brand" header="Brand" sortable style="width: 25%"></Column>
+    <Column field="final_price" header="Price" sortable style="width: 25%"></Column>
+    <Column field="url" header="Link">
+        <template #body="slotProps">
+            <a :href="slotProps.data.url" target="_blank" >Link</a>
+        </template>
+    </Column>
+</DataTable>
+</template>
+
+<script setup>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+defineProps(['data'])
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
