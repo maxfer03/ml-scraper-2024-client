67b80f6b666a4db14b81808efed5ea66acbb0902
Max <sandsferdinand@gmail.com>
2024-01-14 21:07:04 -0300

    added data table

--- src/components/ContentViewerComponent.vue
+++ src/components/ContentViewerComponent.vue
@@ -17,11 +17,8 @@
 import InputText from 'primevue/inputtext';
 import SpinnerComponent from './utility/SpinnerComponent.vue';
 import TableComponent from './TableComponent.vue';
-<<<<<<< HEAD
-=======
 import * as XLSX from 'xlsx/xlsx.mjs';
 import { ref } from 'vue';
->>>>>>> 9fb077d (added data table)
 import axios from 'axios';
 import { ref } from 'vue';
 
