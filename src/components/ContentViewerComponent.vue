c45edea421d2fbf9e670dc7e25545f8176f97153
Max <sandsferdinand@gmail.com>
2024-01-15 11:27:40 -0300

    updated table

--- src/components/ContentViewerComponent.vue
+++ src/components/ContentViewerComponent.vue
@@ -8,10 +8,6 @@
     <div v-if="products.length > 0" class="products-visualizer">
       <span v-if="loading">page {{ reqInfo.current_page }} of {{ reqInfo.total_pages }}</span>
       <span>{{ products.length }} item{{products.length === 1 ? '' : 's'}} found!</span>
-      <div class="files">
-        <Button @click="downloadCSV"  label="Download CSV file" rounded size="small"/>
-        <Button @click="downloadXLSX"  label="Download XLSX file" rounded size="small"/>
-      </div>
       <TableComponent :data="products"/>
     </div>
   </div>
@@ -21,9 +17,9 @@
 import InputText from 'primevue/inputtext';
 import SpinnerComponent from './utility/SpinnerComponent.vue';
 import TableComponent from './TableComponent.vue';
-import * as XLSX from 'xlsx/xlsx.mjs';
-import { ref } from 'vue';
 import axios from 'axios';
+import { ref } from 'vue';
+
 const query = ref('')
 const loading = ref(false)
 
@@ -67,56 +63,6 @@ const search = async () => {
   }
   loading.value = false
 }
-
-function convertToCSV(arr) {
-  const array = [Object.keys(arr[0])].concat(arr);
-
-  return array.map(it => {
-    return Object.values(it).toString();
-  }).join('\n');
-}
-
-function downloadCSV() {
-  const csvData = convertToCSV(products.value);
-  const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
-  const link = document.createElement('a');
-  link.href = URL.createObjectURL(blob);
-  link.setAttribute('download', `${query.value}.csv`);
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
-
-const downloadXLSX = () => {
-  // Create a worksheet
-  const ws = XLSX.utils.json_to_sheet(products.value);
-  // Create a workbook
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-  // Generate a binary string
-  const binaryString = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
-
-  // Convert binary string to character codes
-  const binaryArray = new Uint8Array(binaryString.length);
-  for (let i = 0; i < binaryString.length; i++) {
-    binaryArray[i] = binaryString.charCodeAt(i);
-  }
-
-  // Create a blob from the binary array
-  const blob = new Blob([binaryArray], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-
-  // Create a download link and trigger the download
-  const url = window.URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = `${query.value.replace(' ', "_")}.xlsx`;
-  a.click();
-  window.URL.revokeObjectURL(url);
-}
-
-
-
 
 </script>
 
--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,26 +1,94 @@
 <template>
-<DataTable :value="data" paginator :rows="25" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="width: 1000px"
-scrollable scrollHeight="400px"
->
-    <Column field="title" header="Title" sortable style="width: 25%"></Column>
-    <Column field="brand" header="Brand" sortable style="width: 25%"></Column>
-    <Column field="final_price" header="Price" sortable style="width: 25%"></Column>
-    <Column field="url" header="Link">
-        <template #body="slotProps">
-            <a :href="slotProps.data.url" target="_blank" >Link</a>
-        </template>
+  <DataTable class="table" :value="data" paginator :rows="25" :rowsPerPageOptions="[5, 10, 20, 50]"
+  scrollable scrollHeight="400px" size="small"
+  >
+    <template #header>
+      <div class="table-header" >
+        <Button icon="pi pi-external-link" @click="downloadCSV" label="CSV" />
+        <Button icon="pi pi-external-link" @click="downloadXLSX" label="XLSX" />
+      </div>
+    </template>
+    <Column class="col" field="title" header="Title" sortable style="width: 70%"></Column>
+    <Column class="col" field="brand" header="Brand" sortable style="width: 10%"></Column>
+    <Column class="col" field="final_price" header="Price" sortable style="width: 10%"></Column>
+    <Column class="col" field="url" header="Link" style="width: 10%">
+      <template #body="slotProps">
+          <a class="link" :href="slotProps.data.url" target="_blank" >Link</a>
+      </template>
     </Column>
-</DataTable>
+  </DataTable>
 </template>
 
 <script setup>
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
+import * as XLSX from 'xlsx/xlsx.mjs';
 
-defineProps(['data'])
+const props = defineProps(['data'])
+
+function convertToCSV(arr) {
+  const array = [Object.keys(arr[0])].concat(arr);
+
+  return array.map(it => {
+    return Object.values(it).toString();
+  }).join('\n');
+}
+
+function downloadCSV() {
+  const csvData = convertToCSV(props.data);
+  const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.setAttribute('download', `${'data'}.csv`);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+const downloadXLSX = () => {
+  // Create a worksheet
+  const ws = XLSX.utils.json_to_sheet(props.data);
+  // Create a workbook
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+  // Generate a binary string
+  const binaryString = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+
+  // Convert binary string to character codes
+  const binaryArray = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    binaryArray[i] = binaryString.charCodeAt(i);
+  }
+
+  // Create a blob from the binary array
+  const blob = new Blob([binaryArray], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+
+  // Create a download link and trigger the download
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = `${'data'.replace(' ', "_")}.xlsx`;
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
 
 </script>
 
 <style lang="scss" scoped>
+.table {
+  @apply w-[1000px];
 
+  &-header {
+    @apply flex gap-3;
+  }
+
+  
+
+  .row-title {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
 </style>
