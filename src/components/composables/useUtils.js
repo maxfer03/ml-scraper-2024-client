e4c5df182071cf425206df2e93b432a8c31f299b
Max <sandsferdinand@gmail.com>
2024-03-01 00:35:54 -0300

    prices are converted to USD by default

--- src/components/composables/useUtils.js
+++ src/components/composables/useUtils.js
@@ -1,7 +1,18 @@
+import { useDataStore } from '@/stores/data'
+import { storeToRefs } from 'pinia'
+
+
 export default function useUtils() {
-  const formatPrice = (raw, append='$') => {
-    return (append || '') + Number(raw.toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  const store = useDataStore()
+  const { usdRate, searchInfo} = storeToRefs(store)
+  const dollarize = (crocantes, tld) => {
+    const thisUsdRate = parseFloat(usdRate.value[tld])
+    return crocantes / thisUsdRate
   }
-  
-  return { formatPrice }
+  const formatPrice = (raw, append = '$') => {
+    const rawUsd = dollarize(raw, searchInfo.value.tld)
+    return (append || '') + Number(rawUsd.toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  }
+
+  return { formatPrice, dollarize }
 }
